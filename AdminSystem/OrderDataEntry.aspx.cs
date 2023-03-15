20860e441fde92c66d69dea5aea826360ba006a6
P2616034 <P2616034@JWHX0005WIN007W>
2023-03-15 15:01:21 +0000

    updated other names

--- AdminSystem/OrderDataEntry.aspx.cs
+++ AdminSystem/OrderDataEntry.aspx.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+  
+
+    protected void btnOk_Click(object sender, EventArgs e)
+    {
+        clsOrders AnOrders = new clsOrders();
+        AnOrders.CostumerName = CostumerNo.Text;
+
+        Session["AnOrders"] = AnOrders;
+        Response.Redirect("OrderViewer.aspx");
     }
 }
