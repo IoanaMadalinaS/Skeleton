b23ae87815f128be108cc3554944eba3ad8bfa0d
p2562081 <p2562081@GWHXX281WIN003W>
2023-03-23 19:04:01 +0000

    Created properties to the table

---
+++ ClassLibrary/clsStock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsStock
+    {
+        public bool Active { get; set; }
+        public int ItemN { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string ItemDesc { get; set; }
+        public string ItemName { get; set; }
+        public bool ItemAvaliable { get; set; }
+        public decimal Price { get; set; }
+    }
+}
